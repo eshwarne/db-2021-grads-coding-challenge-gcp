d958954b69ac875067ce5a432fa9d4f194ade4ef
eshwarne <northeasteditsofficial@gmail.com>
2021-08-06 22:01:00 +0530

    DATABASE-CREATION-TASK output password as sensitive

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,10 @@ output "databases-names" {
 }
 output "database-users" { 
      value = google_sql_user.users.*.name
+    description = "user name for grads for each database"
+}
+output "database-password" { 
+     value = google_sql_user.users.*.password
+     description = "Password for grads to use the database as users."
+     sensitive = true
 }
