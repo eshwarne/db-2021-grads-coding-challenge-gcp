3028c87549833e89f0739381d3b38bd156841556
eshwarne <northeasteditsofficial@gmail.com>
2021-08-08 15:47:59 +0530

    DATABASE-CREATION-TASK map user to password java program

--- RandomPasswordGen.java
+++ RandomPasswordGen.java
@@ -1,11 +1,12 @@
+import java.io.FileWriter;
 import java.util.*;
 import java.nio.file.*;
 import java.io.IOException;
 
-class RandomPasswordGen{
+public class RandomPasswordGen{
     public static void main(String[] args){
-        int leftLimit = 97; 
-        int rightLimit = 122; 
+        int leftLimit = 97;
+        int rightLimit = 122;
         int targetStringLength = 10;
         Random random = new Random();
         // List<String> listOf29Passwords = new ArrayList<String>();
@@ -16,15 +17,34 @@ class RandomPasswordGen{
         //         .toString();
         //     listOf29Passwords.add(generatedString);
         // }
-        getPasswordForUser("passwords.txt");
-        //System.out.println(listOf29Passwords);
+        try {
+            String[] passwords = getStringArrayOfEntity("E:\\Deutsche Bank Work Days\\dbgrads2021\\passwords.txt");
+            String[] databases = getStringArrayOfEntity("E:\\Deutsche Bank Work Days\\dbgrads2021\\databases.txt");
+            String[] users = getStringArrayOfEntity("E:\\Deutsche Bank Work Days\\dbgrads2021\\users.txt");
+            String header = String.format("|%30s|        |%20s|        |%20s|","Database Name","User ID","Password");
+            String seperator = String.format("|%30s|        |%20s|        |%20s|","-------------","-------","--------");
+            FileWriter fileWriter = new FileWriter("E:\\Deutsche Bank Work Days\\dbgrads2021\\dbUsersPasswords.txt");
+            fileWriter.write(header);
+            fileWriter.write("\n");
+            fileWriter.write(seperator);
+            fileWriter.write("\n");
+            for(int i=0; i<29; i++){
+                String formatted = String.format(" %30s         %20s         %20s",databases[i],users[i],passwords[i]);
+                fileWriter.write(formatted);
+                fileWriter.write("\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void getPasswordForUser(String filePathString){
-        Map<String, String> userPasswordTable = new HashMap<>();
+    public static String[] getStringArrayOfEntity(String filePathString) throws IOException {
         Path filePath = Path.of(filePathString);
         String content = Files.readString(filePath);
-        System.out.println(content);
+        String[] entity = content.split(", ");
+        return entity;
+
     }
 }
 
