a283bd401bb2ef1fb8f14e3ed5ccf515f292c75c
Sarath Kaul <kaul.sarath@gmail.com>
2021-08-10 11:56:06 +0530

    Update roles.sh

--- shell/roles.sh
+++ shell/roles.sh
@@ -2,16 +2,16 @@
 
 
 # Add Project Ids of all 29 Projects
-declare -a projects=("element1" "element2" "element3")
+declare -a projects=("grads-coding-challenge-group-1")
 
 for i in "${projects[@]}"
 do
   # Setting Batch Project
   gcloud config set project $i
   # Enabling All APIs
-  gcloud services enable cloudresourcemanager.googleapis.com serviceusage.googleapis.com logging.googleapis.com appengine.googleapis.com
+  gcloud services enable cloudresourcemanager.googleapis.com serviceusage.googleapis.com logging.googleapis.com appengine.googleapis.com cloudbuild.googleapis.com
   # CReating App Engine
-  gcloud app create --region=us-central
+  gcloud app create --region=us-central || echo "Already Created"
   
   # Assigning roles to Cloud Build Service Account
   number=$(gcloud projects list --filter="$i" --format="value(PROJECT_NUMBER)")
@@ -30,5 +30,5 @@ for i in "${projects[@]}"
 do
     # Assigning Roles to Batch Project App Engine SA
     sa="${i}@appspot.gserviceaccount.com"
-    gcloud projects add-iam-policy-binding $i --member="serviceAccount:${sa}" --role='roles/cloudsql.client'
+    gcloud projects add-iam-policy-binding grad-coding-challenge-1 --member="serviceAccount:${sa}" --role='roles/cloudsql.client'
 done
